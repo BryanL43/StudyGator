54823ab63f060f2aea6fd044efe358bd2fee7f90
Bryan Lee <62684188+BryanL43@users.noreply.github.com>
2024-09-20 16:37:09 -0700

    m0 force redirect home to About page

--- application/client/src/App.jsx
+++ application/client/src/App.jsx
@@ -16,7 +16,7 @@ import { AuthProvider } from './AuthContext';
 import DefaultLayout from './layouts/DefaultLayout.jsx';
 
 // Import the subpages
-import Home from "./Home/Home.jsx";
+// import Home from "./Home/Home.jsx";
 import About from "./about/AboutPage.jsx";
 
 function App() {
@@ -31,7 +31,8 @@ function App() {
 
                         {/* Default Routes */}
                         <Route element={<DefaultLayout />}>
-                            <Route path="/" element={<Home />} />
+                            <Route path="/" element={<About />} /> {/* Force redirect to about page for m0 */}
+                            {/* <Route path="/" element={<Home />} /> */}
                             <Route path="/about" element={<About />} />
                             <Route path="/about/:name" element={<About />} />
                         </Route>
--- application/server/server.js
+++ application/server/server.js
@@ -15,7 +15,7 @@ const upload = multer({ storage: storage });
 
 
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000, http://ec2-54-176-10-43.us-west-1.compute.amazonaws.com/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
     credentials: true,
