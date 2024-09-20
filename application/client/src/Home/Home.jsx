b41f0810ce9ba2b19515c9fe39acb058ae772b82
Bryan Lee <62684188+BryanL43@users.noreply.github.com>
2024-09-20 15:44:50 -0700

    Added temp about page link

--- application/client/src/Home/Home.jsx
+++ application/client/src/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <Link to="/about" className="text-pink-400">About Page</Link>
+    )
 }
 
 export default Home
