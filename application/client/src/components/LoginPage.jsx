49c865ec67d43cf8353c668fda6fc607ae0c9b78
Bryan Lee <62684188+BryanL43@users.noreply.github.com>
2024-11-02 12:47:18 -0700

    Detail touch up on login page

--- application/client/src/components/LoginPage.jsx
+++ application/client/src/components/LoginPage.jsx
@@ -43,22 +43,24 @@ const LoginPage = () => {
     };
 
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
-            <div className="w-full max-w-md bg-white dark:bg-gray-800 p-8 rounded-lg shadow-md">
-                <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-6 text-center">Login</h2>
+        <div className="top-0 flex items-center justify-center sm:min-h-screen bg-gray-100">
+            <div className="absolute inset-0 sm:top-[72px] h-full bg-black opacity-20 z-20 sm:block hidden" />
+            <img src="/SFSU-img-4.png" className="absolute w-full h-full object-cover z-10 sm:block hidden" alt="Bird eye view of SFSU" />
+            <div className="w-full sm:max-w-md bg-white p-8 sm:rounded-lg sm:shadow-md z-30">
+                <h2 className="text-2xl font-bold text-gray-800 mb-6 text-center">Login</h2>
 
                 <form onSubmit={handleSubmit} className="space-y-6">
                     <div>
-                        <label htmlFor="email" className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+                        <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                             SFSU Email
                         </label>
-                        <div className="mt-1 flex items-center border border-gray-300 dark:border-gray-700 rounded-md shadow-sm">
-                            <MdPerson className="ml-2 text-gray-400" size={30} />
+                        <div className="mt-1 flex items-center border border-gray-300 rounded-md shadow-sm">
+                            <MdPerson className="ml-2 mr-2 text-gray-400" size={30} />
                             <input
                                 type="text"
                                 id="email"
                                 name="email"
-                                className="block w-full p-2 border-none focus:ring-transparent dark:bg-gray-700 dark:text-gray-200"
+                                className="block w-full p-2 border-none focus:ring-transparent"
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
                                 required
@@ -67,16 +69,16 @@ const LoginPage = () => {
                     </div>
 
                     <div>
-                        <label htmlFor="password" className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+                        <label htmlFor="password" className="block text-sm font-medium text-gray-700">
                             Password
                         </label>
-                        <div className="mt-1 flex items-center border border-gray-300 dark:border-gray-700 rounded-md shadow-sm">
-                            <MdLock className="ml-2 text-gray-400" size={30} />
+                        <div className="mt-1 flex items-center border border-gray-300rounded-md shadow-sm">
+                            <MdLock className="ml-2 mr-2 text-gray-400" size={30} />
                             <input
                                 type="password"
                                 id="password"
                                 name="password"
-                                className="block w-full p-2 border-none focus:ring-transparent dark:bg-gray-700 dark:text-gray-200"
+                                className="block w-full p-2 border-none focus:ring-transparent"
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
                                 required
@@ -91,11 +93,11 @@ const LoginPage = () => {
                         <input
                             type="checkbox"
                             id="rememberMe"
-                            className="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-400"
+                            className="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
                             checked={rememberMe}
                             onChange={(e) => setRememberMe(e.target.checked)}
                         />
-                        <label htmlFor="rememberMe" className="ml-2 block text-sm text-gray-900 dark:text-gray-300">
+                        <label htmlFor="rememberMe" className="ml-2 block text-sm text-gray-900">
                             Remember Me
                         </label>
                     </div>
@@ -106,7 +108,7 @@ const LoginPage = () => {
                         </span>
                         <button
                             type="submit"
-                            className="bg-[#231161] hover:bg-[#1f0e55] text-white font-medium rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#231161] focus:ring-offset-2 dark:focus:ring-offset-gray-800"
+                            className="bg-[#231161] hover:bg-[#1f0e55] text-white font-medium rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#231161] focus:ring-offset-2"
                         >
                             Login
                         </button>
