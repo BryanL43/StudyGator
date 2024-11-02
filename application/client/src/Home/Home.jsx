0f0b4cf713205885e2ab7d0e02e20b917d886f88
Bryan Lee <62684188+BryanL43@users.noreply.github.com>
2024-11-02 09:25:47 -0700

    Implemented landing page image carousel

--- application/client/src/Home/Home.jsx
+++ application/client/src/Home/Home.jsx
@@ -4,167 +4,182 @@
 *
 * File:: Home.jsx
 *
-* Description:: This file handles the vertical prototype frontend page.
-*               It displays the tutor listings and allows for tutor application
-*               if the user is logged in. [WARNING: file is mainly disposable prototype]
+* Description:: The landing page for the application containing a image carousel
+*               and a list of recent tutor listings.
 *
 **************************************************************/
 
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Search from './Search';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
+import imageIcon from './ImageIcon.svg'; // Special image icon
 
-import { useAuth } from '../AuthContext.js';
-import BASE_URL from '../utils/config';
+// Timer logic for handling manual interruption to automatic image rotation
+const useTimer = (callback, delay) => {
+    const intervalRef = useRef(null);
+
+    const startTimer = useCallback(() => {
+        clearInterval(intervalRef.current);
+        intervalRef.current = setInterval(callback, delay);
+    }, [callback, delay]);
+
+    const clearTimer = useCallback(() => {
+        clearInterval(intervalRef.current);
+    }, []);
+
+    // Start the timer on mount and clear it on unmount
+    useEffect(() => {
+        startTimer();
+        return clearTimer;
+    }, [startTimer, clearTimer]);
+
+    return { resetTimer: startTimer, stopTimer: clearTimer };
+};
 
 const Home = () => {
-    const { user } = useAuth(); // Retrieve user token. Critical for login infrastructure.
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const [buttonHeight, setButtonHeight] = useState(0);
+    const [overlayState, setOverlayState] = useState(false);
+    const imageRef = useRef(null);
 
-    const [selectedFile, setSelectedFile] = useState(null);
-    const [description, setDescription] = useState('');
-    const [subjectId, setSubjectId] = useState('');
-    const [listings, setListings] = useState([]);
-    const [pricing, setPricing] = useState('');
-    const [subjectList, setSubjectList] = useState([]); // For drop down
-
-    // Fetch subjects to populate dropdown for applying as tutor
-    const fetchSubjects = async() => {
-        try {
-            const response = await axios.get(`${BASE_URL}/api/subject`);
-            setSubjectList(response.data);
-        } catch (error) {
-            console.error("Error fetching subjects:", error);
-        }
-    };
-
-    // Render default listings upon landing
-    const fetchListings = async() => {
-        try {
-            const response = await axios.get(`${BASE_URL}/api/search`);
-            setListings(response.data.results);
-        } catch (error) {
-            console.error("Error fetching listings:", error);
+    // Update button sizes based on image size for very small screen
+    const updateButtonHeight = () => {
+        const img = imageRef.current;
+        if (img ) {
+            setButtonHeight(img.clientHeight);
         }
     };
 
     useEffect(() => {
-        fetchSubjects();
-        fetchListings();
+        updateButtonHeight();
+
+        window.addEventListener('resize', updateButtonHeight); // Update on resize
+        return () => { // Clean up listener
+            window.removeEventListener('resize', updateButtonHeight);
+        };
     }, []);
 
-    // Acquire uploaded image
-    const handleFileChange = (e) => {
-        setSelectedFile(e.target.files[0]);
-    };
+    // Function to go to the next slide
+    const nextSlide = () => {
+        setOverlayState(true);
 
-    // Handle form submission for tutor application
-    const handleSubmit = async(e) => {
-        e.preventDefault();
-
-        if (!selectedFile || !description || !subjectId || !pricing) {
-            alert("Please fill all required fields.");
-            return;
-        }
-
-        const formData = new FormData();
-        formData.append('image', selectedFile);
-        formData.append('description', description);
-        formData.append('subjectId', subjectId);
-        formData.append('pricing', pricing);
-
-        try {
-            await axios.put(`${BASE_URL}/api/apply`, formData, {
-                headers: {
-                    'Authorization': localStorage.getItem("authToken"),
-                    'Content-Type': 'multipart/form-data',
+        setTimeout(() => {
+            setCurrentSlide((prevSlide) => {
+                if (prevSlide + 1 >= 3) {
+                    return 0;
                 }
+                return prevSlide + 1;
             });
-            window.location.reload(false);
-            fetchListings();
-        } catch (error) {
-            console.error("Error uploading listing:", error);
-        }
+            setOverlayState(false);
+        }, 400);
     };
+
+    const { resetTimer } = useTimer(nextSlide, 6000);
+
+    // Function to go to the previous slide
+    const prevSlide = () => {
+        setOverlayState(true);
+
+        setTimeout(() => {
+            setCurrentSlide((prevSlide) => {
+                if (prevSlide - 1 < 0) {
+                    return 2;
+                }
+                return prevSlide - 1;
+            });
+            setOverlayState(false);
+            resetTimer();
+        }, 400);
+    };
+
+    // Direct slide jump
+    const setSlide = (slideNum) => {
+        setOverlayState(true);
+
+        setTimeout(() => {
+            setCurrentSlide(slideNum);
+            setOverlayState(false);
+            resetTimer();
+        }, 400);
+    }
 
     return (
         <div>
-            <header className="text-center mb-5">
-                <h1 className="font-bold text-2xl">CSC 648-848 Software Engineering</h1>
-                <h2 className="font-bold text-xl">Fall 2024</h2>
-                <h3 className="font-bold text-lg">Team 08 - Vertical Prototype</h3>
-            </header>
-
-            {/* Render apply as tutor form if user is logged in */}
-            {user && (
-                <form onSubmit={handleSubmit} className="text-center">
-                    <input 
-                        type="file" 
-                        onChange={handleFileChange} 
-                        required 
-                    />
-                    <input 
-                        type="text" 
-                        className="border border-black mb-2 p-2 mr-2"
-                        value={description} 
-                        onChange={(e) => setDescription(e.target.value)} 
-                        placeholder="Description" 
-                        required 
-                    />
-                    <select 
-                        value={subjectId} 
-                        onChange={(e) => setSubjectId(e.target.value)} 
-                        required
-                        className="border border-black mb-2 p-2 mr-2"
-                    >
-                        <option value="">Select Subject</option>
-                        {subjectList.map((subject) => (
-                            <option key={subject.id} value={subject.id}>
-                                {subject.name}
-                            </option>
-                        ))}
-                    </select>
-                    <input 
-                        type="text" 
-                        className="border border-black mb-2 p-2 mr-2"
-                        value={pricing} 
-                        onChange={(e) => setPricing(e.target.value)} 
-                        placeholder="Pricing" 
-                        required 
-                    />
-                    <button type="submit" className="bg-blue-500 text-white px-4 py-1 rounded hover:bg-blue-600"> Upload </button>
-                </form>
-            )}
-
-            <br />
-            <Search/>
-            <br />
-            
-            {/* Render listing count */}
-            {listings && listings.length > 0 ? (
-                <h2 className="text-m font-semibold mt-4 ml-52"> Showing {listings.length} listings:</h2>
-            ) : (
-                <h2 className="text-m font-semibold mt-4 ml-52"> No listings currently exist.</h2>
-            )}
-
-            {/* Default render all listings */}
-            {listings ? (
-                listings.map((listing, index) => (
-                    <div key={index} className="text-center">
-                        <div className="flex flex-col items-center mb-3">
-                            <p>Name: {listing.tutorName}</p>
-                            <p>Description: {listing.description}</p>
-                            <p>Subject: {listing.subjectName}</p>
-                            <p>Pricing: ${listing.pricing} per hour</p>
-                            {listing.image && (
-                                <img src={listing.image} alt="Listing" className="w-24 h-24" />
-                            )}
-                        </div>
-                        <hr className="bg-black h-0.5 border-none mb-3" />
+            {/* Image carousel */}
+            <div id="default-carousel" className="relative w-full" data-carousel="slide">
+                <div className="relative h-[70vh] overflow-hidden">
+                    <div className={`duration-700 ease-in-out z-10 ${currentSlide === 0 ? 'block' : 'hidden'}`} data-carousel-item>
+                        <img 
+                            src="/SFSU-img-1.jpg" 
+                            ref={imageRef} 
+                            className="absolute w-full h-full object-cover -translate-x-1/2 sm:-translate-y-1/2 sm:top-1/2 left-1/2" 
+                            alt="SFSU library" 
+                        />
                     </div>
-                ))
-            ) : (
-                <p className="text-center">No listings available.</p>
-            )}
+                    <div className={`duration-700 ease-in-out z-10 ${currentSlide === 1 ? 'block' : 'hidden'}`} data-carousel-item>
+                        <img 
+                            src="/SFSU-img-2.jpg" 
+                            className="absolute w-full h-full object-cover -translate-x-1/2 sm:-translate-y-1/2 sm:top-1/2 left-1/2" 
+                            alt="Night time SFSU library" 
+                        />
+                    </div>
+                    <div className={`duration-700 ease-in-out z-10 ${currentSlide === 2 ? 'block' : 'hidden'}`} data-carousel-item>
+                        <img 
+                            src="/SFSU-img-3.jpg" 
+                            className="absolute w-full h-full object-cover -translate-x-1/2 sm:-translate-y-1/2 sm:top-1/2 left-1/2" 
+                            alt="SFSU entrance" 
+                        />
+                    </div>
+                </div>
+
+                {/* Overlay header */}
+                <div className="absolute z-40 w-[70%] left-[17.5%] sm:left-[22.5%] top-[45%] transform -translate-y-1/2">
+                    <h1 className="text-[48px] text-white font-bold drop-shadow-lg">
+                        Find Your SFSU Tutor at<br></br>StudyGator
+                    </h1>
+                </div>
+
+                {/* Overlay for transition effect */}
+                <div className={`absolute inset-0 transition-all duration-500 ${overlayState ? 'bg-white opacity-90' : 'bg-black opacity-20'}`} />
+
+                {/* Default image carousel navigation */}
+                <div className="absolute hidden sm:block left-[20%] top-1/2 transform -translate-y-1/2 z-40">
+                    <ol className="border-s-[2px] border-white">
+                        <li className="mb-10 ms-6 h-16 flex items-center" onClick={() => setSlide(0)} >
+                            <span className="absolute flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full -start-4 ring-4 ring-white hover:ring-[#231161] hover:bg-[#ffdc70] cursor-pointer">
+                                <img src={imageIcon} className="w-3.5 h-3.5" alt="" />
+                            </span>
+                        </li>
+                        <li className="mb-10 ms-6 h-16 flex items-center" onClick={() => setSlide(1)}>
+                            <span className="absolute flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full -start-4 ring-4 ring-white hover:ring-[#231161] hover:bg-[#ffdc70] cursor-pointer">
+                                <img src={imageIcon} className="w-3.5 h-3.5" alt="" />
+                            </span>
+                        </li>
+                        <li className="mb-10 ms-6 h-16 flex items-center" onClick={() => setSlide(2)}>
+                            <span className="absolute flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full -start-4 ring-4 ring-white hover:ring-[#231161] hover:bg-[#ffdc70] cursor-pointer">
+                                <img src={imageIcon} className="w-3.5 h-3.5" alt="" />
+                            </span>
+                        </li>
+                    </ol>
+                </div>
+
+                {/* Small screen image carousel navigation */}
+                <button type="button" onClick={prevSlide} className="absolute sm:hidden top-0 start-0 z-30 flex items-center justify-center px-4 cursor-pointer group focus:outline-none" style={{ height: buttonHeight }} data-carousel-prev>
+                    <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30  group-hover:bg-white/50  group-focus:ring-4 group-focus:ring-white  group-focus:outline-none">
+                        <svg className="w-4 h-4 text-white  rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
+                        </svg>
+                        <span className="sr-only">Previous</span>
+                    </span>
+                </button>
+                <button type="button" onClick={nextSlide} className="absolute sm:hidden top-0 end-0 z-30 flex items-center justify-center px-4 cursor-pointer group focus:outline-none" style={{ height: buttonHeight }} data-carousel-next>
+                    <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30  group-hover:bg-white/50  group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
+                        <svg className="w-4 h-4 text-white  rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
+                        </svg>
+                        <span className="sr-only">Next</span>
+                    </span>
+                </button>
+            </div>
+
         </div>
     );
 };
