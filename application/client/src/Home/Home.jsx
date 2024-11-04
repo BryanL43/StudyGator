e1d2c2cf8dc49092d188b9373368ec3b32df825d
Bryan Lee <62684188+BryanL43@users.noreply.github.com>
2024-11-04 04:39:26 -0800

    Shedded unnecessary features

--- application/client/src/Home/Home.jsx
+++ application/client/src/Home/Home.jsx
@@ -10,7 +10,8 @@
 **************************************************************/
 
 import React, { useEffect, useState, useRef, useCallback } from 'react';
-import imageIcon from './ImageIcon.svg'; // Special image icon
+import imageIcon from './ImageIcon.svg';
+import loadingIcon from './LoadingIcon.svg';
 
 import TutorListingCard from '../components/TutorListingCard';
 
@@ -38,29 +39,11 @@ const useTimer = (callback, delay) => {
 
 const Home = () => {
     const [currentSlide, setCurrentSlide] = useState(0);
-    const [buttonHeight, setButtonHeight] = useState(0);
     const [overlayState, setOverlayState] = useState(false);
     const [isLocked, setIsLocked] = useState(false);
     const imageRef = useRef(null);
 
-    // Update button sizes based on image size for very small screen
-    const updateButtonHeight = () => {
-        const img = imageRef.current;
-        if (img && img.clientHeight !== 0) {
-            setButtonHeight(img.clientHeight);
-        }
-    };
-
-    useEffect(() => {
-        updateButtonHeight();
-
-        window.addEventListener('resize', updateButtonHeight); // Update on resize
-        return () => { // Clean up listener
-            window.removeEventListener('resize', updateButtonHeight);
-        };
-    }, []);
-
-    // Function to go to the next slide
+    // Function to switch to the next slide
     const nextSlide = () => {
         if (isLocked) return;
         setOverlayState(true);
@@ -78,37 +61,11 @@ const Home = () => {
             // Lock buttons for 1.5 seconds
             setTimeout(() => {
                 setIsLocked(false);
-                updateButtonHeight();
             }, 1500);
         }, 400);
-        updateButtonHeight();
     };
 
     const { resetTimer } = useTimer(nextSlide, 6000);
-
-    // Function to go to the previous slide
-    const prevSlide = () => {
-        if (isLocked) return;
-        setOverlayState(true);
-        setIsLocked(true);
-
-        setTimeout(() => {
-            setCurrentSlide((prevSlide) => {
-                if (prevSlide - 1 < 0) {
-                    return 2;
-                }
-                return prevSlide - 1;
-            });
-            setOverlayState(false);
-            resetTimer();
-
-            // Lock buttons for 1.5 seconds
-            setTimeout(() => {
-                setIsLocked(false);
-            }, 1500);
-        }, 400);
-        updateButtonHeight();
-    };
 
     // Direct slide jump
     const setSlide = (slideNum) => {
@@ -126,7 +83,6 @@ const Home = () => {
                 setIsLocked(false);
             }, 1500);
         }, 400);
-        updateButtonHeight();
     }
 
     return (
@@ -161,62 +117,50 @@ const Home = () => {
                 {/* Overlay header */}
                 <div className="absolute z-40 w-[70%] left-[17.5%] sm:left-[25.5%] md:left-[22.5%] top-[45%] transform -translate-y-1/2">
                     <h1 className="text-[48px] text-white font-bold drop-shadow-lg">
-                        Find Your SFSU Tutor at<br></br>StudyGator
+                        Find Your SFSU Tutor at<br></br>StudyGator!
                     </h1>
                 </div>
 
                 {/* Overlay for transition effect */}
                 <div className={`absolute inset-0 transition-all duration-500 ${overlayState ? 'bg-white opacity-90' : 'bg-black opacity-20'}`} />
 
-                {/* Default image carousel navigation */}
+                {/* Image carousel navigation */}
                 <div className="absolute hidden sm:block left-[20%] top-1/2 transform -translate-y-1/2 z-40">
                     <ol className="border-s-[2px] border-white">
                         <li className="mb-10 ms-6 h-16 flex items-center" onClick={() => setSlide(0)} >
-                            <span className="absolute flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full -start-4 ring-4 ring-white hover:ring-[#231161] hover:bg-[#ffdc70] cursor-pointer">
+                            <span className={`absolute flex items-center justify-center w-8 h-8 ${(currentSlide === 0) ? 'ring-[#231161] bg-[#ffdc70]' : 'ring-white bg-gray-100 hover:ring-gray-50 hover:bg-gray-300'} rounded-full -start-4 ring-4 ring-white cursor-pointer`}>
                                 <img src={imageIcon} className="w-3.5 h-3.5" alt="" />
                             </span>
                         </li>
                         <li className="mb-10 ms-6 h-16 flex items-center" onClick={() => setSlide(1)}>
-                            <span className="absolute flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full -start-4 ring-4 ring-white hover:ring-[#231161] hover:bg-[#ffdc70] cursor-pointer">
+                            <span className={`absolute flex items-center justify-center w-8 h-8 ${(currentSlide === 1) ? 'ring-[#231161] bg-[#ffdc70]' : 'ring-white bg-gray-100 hover:ring-gray-50 hover:bg-gray-300'} rounded-full -start-4 ring-4 ring-white cursor-pointer`}>
                                 <img src={imageIcon} className="w-3.5 h-3.5" alt="" />
                             </span>
                         </li>
                         <li className="mb-10 ms-6 h-16 flex items-center" onClick={() => setSlide(2)}>
-                            <span className="absolute flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full -start-4 ring-4 ring-white hover:ring-[#231161] hover:bg-[#ffdc70] cursor-pointer">
+                        <span className={`absolute flex items-center justify-center w-8 h-8 ${(currentSlide === 2) ? 'ring-[#231161] bg-[#ffdc70]' : 'ring-white bg-gray-100 hover:ring-gray-50 hover:bg-gray-300'} rounded-full -start-4 ring-4 ring-white cursor-pointer`}>
                                 <img src={imageIcon} className="w-3.5 h-3.5" alt="" />
                             </span>
                         </li>
                     </ol>
                 </div>
 
-                {/* Small screen image carousel navigation */}
-                <button type="button" onClick={prevSlide} className="absolute sm:hidden top-0 start-0 z-30 flex items-center justify-center px-4 cursor-pointer group focus:outline-none" style={{ height: buttonHeight }} data-carousel-prev>
-                    <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30  group-hover:bg-white/50  group-focus:ring-4 group-focus:ring-white  group-focus:outline-none">
-                        <svg className="w-4 h-4 text-white  rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
-                        </svg>
-                        <span className="sr-only">Previous</span>
-                    </span>
-                </button>
-                <button type="button" onClick={nextSlide} className="absolute sm:hidden top-0 end-0 z-30 flex items-center justify-center px-4 cursor-pointer group focus:outline-none" style={{ height: buttonHeight }} data-carousel-next>
-                    <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30  group-hover:bg-white/50  group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
-                        <svg className="w-4 h-4 text-white  rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
-                        </svg>
-                        <span className="sr-only">Next</span>
-                    </span>
-                </button>
             </div>
             
             {/* Recent Listings */}
             <section className="bg-gray-50 py-8 antialiased md:py-12">
                 <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
-                    <div className="mb-4 flex items-end justify-center space-y-4 sm:space-y-0 md:mb-8">
+                    <div className="mb-12 flex items-end justify-center space-y-4 sm:space-y-0 md:mb-16">
                         <h2 className="mt-3 text-[28px] font-bold text-gray-900">Recent Tutor Listings</h2>
                     </div>
 
+                    {/* Loading icon */}
+                    <div className="hidden flex items-center justify-center">
+                        <img src={loadingIcon} className="w-20 h-20" alt="Loading..." />
+                    </div>
+
                     {/* Grid layout with 4 columns for the listing cards*/}
-                    <div className="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 justify-center">
+                    <div className="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 justify-center justify-items-center">
 
                         <TutorListingCard
                             name="Bryan Lee"
