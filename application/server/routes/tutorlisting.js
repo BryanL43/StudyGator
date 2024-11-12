5dbfd74fc61dde65e0d9a048164c5c617425ec16
Nishi <113165182+nishisuratia@users.noreply.github.com>
2024-11-11 18:32:15 -0800

    Update tutorlisting.js fixing routes

--- application/server/routes/tutorlisting.js
+++ application/server/routes/tutorlisting.js
@@ -1,27 +1,16 @@
+/**************************************************************
+* Author(s): Nishi Suratia
+* Last Updated: 11/11/2024
+*
+* File:: tutorlisting.js
+*
+*.
+*
+**************************************************************/
 const express = require('express');
 const router = express.Router();
-const connectDB = require('../config/db');
+const { getTutorListingsByUserHandler } = require('../controller/listingController');
 
-router.get('/api/tutor-listings', async (req, res) => {
-    try {
-        const pool = await connectDB();
-        const [results] = await pool.query(`
-            SELECT 
-                id, 
-                sales_pitch, 
-                description, 
-                pricing, 
-                image, 
-                attached_video, 
-                attached_file, 
-                date_created 
-            FROM TUTORLISTINGS;
-        `);
-        res.json(results);
-    } catch (error) {
-        console.error('Error fetching tutor listings:', error);
-        res.status(500).json({ error: 'Database query error' });
-    }
-});
+router.get("/", getTutorListingsByUserHandler);
 
 module.exports = router;
