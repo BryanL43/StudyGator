a574e098c2cf047830a9a0210250cdf08551d4d9
Nishi <113165182+nishisuratia@users.noreply.github.com>
2024-11-14 12:12:52 -0800

    added routes for messages

--- application/server/routes/messageroutes.js
+++ application/server/routes/messageroutes.js
@@ -1,1 +1,18 @@
+/**************************************************************
+* Author(s): Nishi Suratia
+* Last Updated: 14/11/2024
+*
+* File:: messageroutes.js
+*
+*
+*
+**************************************************************/
 
+const express = require('express');
+const router = express.Router();
+const {sendMessageHandler }= require('../controller/listingController');
+
+// Route to send a message
+router.post('/send', sendMessageHandler);
+
+module.exports = router;
