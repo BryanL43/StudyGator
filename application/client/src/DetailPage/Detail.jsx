ede0dd7aa9c6046de7f58e7c7df8d5c511f83205
Kenwen1 <kwen@sfsu.edu>
2024-12-03 16:40:15 -0800

    Added white background to detail page for profile pics

--- application/client/src/DetailPage/Detail.jsx
+++ application/client/src/DetailPage/Detail.jsx
@@ -70,7 +70,7 @@ const Detail = () => {
 
             <div className="relative w-full flex justify-center -mt-20">
                 {/* Fixed-Size Container for the Profile Picture */}
-                <div className="w-80 h-80 rounded-md shadow-xl overflow-hidden">
+                <div className="w-80 h-80 rounded-md shadow-xl overflow-hidden bg-white">
                     <img
                         src={tutor?.image || '/default-profile.png'}
                         alt={`${tutor?.tutorName}'s profile`}
