f28d056cf59b33d7e4c50d941a6221010ca063bd
bryan lee <bryanlee56098@gmail.com>
2024-12-08 21:50:57 -0800

    Fixed a typo

--- application/client/src/about/AboutPage.jsx
+++ application/client/src/about/AboutPage.jsx
@@ -76,7 +76,7 @@ const Team = () => {
             <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
                 <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">Our Team</h2>
                 <p className="font-light text-gray-500 lg:mb-16 sm:text-xl">
-                    This is Team#8's group project for 648/848 Software Engineering className. As a team, innovating and delivering
+                    This is Team#8's group project for 648/848 Software Engineering class. As a team, innovating and delivering
                     high-quality solutions for clients and users alike.
                 </p>
             </div>
